60f2a3b04ce2d9e219a5db64b7b7de86c5bb1081
alessandroCassani <acassani@studenti.uninsubria.it>
2023-07-19 18:01:58 +0200

    switch metodi

--- vue_auth/frontend/performanceTesting.js
+++ vue_auth/frontend/performanceTesting.js
@@ -1,6 +1,8 @@
+const { create } = require('core-js/core/object');
 const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
 let numeroTask = 0;
 let numeroDrug = 0; 
+
 async function insertMemo() {
   const memoCiphered = {
           evento: 'guardia medica' + numeroTask++,
@@ -8,7 +10,10 @@ async function insertMemo() {
           orario: '15:56',
           email_paziente: '771c2c3afda9151482bee26ec7052f98',
         };
+  const startTime = Date.now()
   await axios.post("http://localhost:5002/insertMemo", memoCiphered)
+  const endTime = Date.now()
+  return endTime- startTime
 }
 
 async function insertDrug() {
@@ -18,9 +23,10 @@ async function insertDrug() {
           dosaggio: '150',
           email_paziente: '771c2c3afda9151482bee26ec7052f98',
         };
-
-        await axios
-          .post("http://localhost:5002/insertTherapy", medicinaleCiphered)
+const startTime = Date.now()
+await axios.post("http://localhost:5002/insertTherapy", medicinaleCiphered)
+const endTime = Date.now()
+return endTime - startTime;
 }
 
 async function deleteDrug() {
@@ -28,10 +34,11 @@ async function deleteDrug() {
           farmaco: 'aulin' + --numeroDrug,
           email: '771c2c3afda9151482bee26ec7052f98',
         };
-
-        await axios
-          .post("http://localhost:5002/deleteDrug", medicinale)
-          numeroDrug++
+  const startTime = Date.now()
+  await axios.post("http://localhost:5002/deleteDrug", medicinale)
+  const endTIme = Date.now()
+  numeroDrug++
+  return endTIme - startTime
 }
 
 async function deleteTask() {
@@ -39,10 +46,156 @@ async function deleteTask() {
           evento: 'guardia medica' + --numeroTask,
           email: '771c2c3afda9151482bee26ec7052f98',
         };
+  const startTime = Date.now()
+  await axios.post("http://localhost:5002/deleteTask", task)
+  const endTime = Date.now()
+  numeroTask++
+  return endTime-startTime
+}
 
-        await axios
-          .post("http://localhost:5002/deleteTask", task)
-          numeroTask++
+async function getMedia() {
+  const data = {
+    collection: '771c2c3afda9151482bee26ec7052f98' + '/vitalparameters',
+    firstDate: '16/07/2023',
+    secondDate: '18/08/2023',
+    parametro: 'fc',
+  };
+  const startTime = Date.now()
+  await axios.post("http://localhost:5005/getMedia", data)
+  const endTime = Date.now()
+  return endTime-startTime
+}
+
+async function getPazientiHome(){
+  const email = {
+    email_caregiver: '3cf69e2d70eedc2100b8d2b303d49792',
+  };
+  const startTime = Date.now()
+  await axios.post("http://localhost:5001/home", email)
+  const endTime = Date.now()
+  return endTime-startTime
+}
+
+async function getInfoUser(){
+  const data = {
+    email: '3cf69e2d70eedc2100b8d2b303d49792',
+  };
+  const startTime = Date.now()
+  await axios.post("http://localhost:5000/user", data);
+  const endTime = Date.now()
+  return endTime-startTime
+}
+
+
+async function insertAlerts(){
+  const data = {
+    email: '771c2c3afda9151482bee26ec7052f98',
+    fc: '120',
+    spO2: '97',
+    systolic: '150',
+    diastolic: '110',
+  };
+  const startTime = Date.now()
+  await axios.post("http://localhost:5005/insertAlerts", data)
+  const endTime = Date.now()
+  return endTime-startTime
+}
+
+
+let otp = 10000
+async function createOtp(){
+  otp = otp++
+  const data = {
+    otp: otp.toString(),
+    email: sessionStorage.getItem("email"),
+  };
+  const startTime = Date.now()
+  axios.post("http://localhost:5001/insertOtp", data)
+  const endTime = Date.now()
+  return endTime-startTime
+}
+
+async function checkOtp(){
+  const data = {
+    otp: otp.toString(),
+    email_paziente: '771c2c3afda9151482bee26ec7052f98',
+    email_caregiver: '3cf69e2d70eedc2100b8d2b303d49792',
+  };
+  const startTime = Date.now()
+  axios.post("http://localhost:5001/checkOtp", data)
+  const endTime = Date.now()
+  return endTime-startTime
+}
+
+async function getDataFromMongoDb(){
+  const rifPaziente = '771c2c3afda9151482bee26ec7052f98';
+  const collezione = rifPaziente + "/vitalparameters";
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+  let param;
+  if(randomNumber === 1){
+      param = 'fc'
+  }else{
+    if(randomNumber === 2){
+      param = 'spO2'
+    }else{
+      param = 'bp'
+    }
+  }
+
+  let data = {
+    field: param,
+    collection: collezione,
+  };
+
+  if (param === "bp") {
+    data = {
+      field: "systolic",
+      field2: "diastolic",
+      collection: collezione,
+    };
+  }
+  const startTime = Date.now()
+  await axios.get("http://localhost:5005/getData", { params: data })
+  const endTime = Date.now()
+  return endTime-startTime
+}
+
+
+async function insertPvs(){
+  let object = {
+    fc: '120',
+    spO2: '98',
+    systolic: '130',
+    diastolic: '100',
+    collection: '771c2c3afda9151482bee26ec7052f98 '+ "/vitalparameters",
+  };
+  const startTime = Date.now()
+  await axios.post("http://localhost:5005/insertPv", object)
+  const endTime = Date.now()
+  return endTime-startTime
+}
+
+let signUpNumber = 0;
+async function signUp(){
+  let ruoloUser
+  if(signUpNumber%2==0){
+     ruoloUser = 'paziente'
+  }else{
+     ruoloUser = 'caregiver'
+  }
+  let newUser = {
+    nome: 'ale',
+    cognome: 'cas',
+    dataDiNascita: '12/02/1998',
+    email: 'cassa@gmail.com)' + signUpNumber++,
+    password: 'ciao',
+    ripetiPassword: 'ciao',
+    ruolo: ruoloUser,
+  };
+  const startTime = Date.now()
+  await axios.post("http://localhost:5000/signup", newUser);
+  const endTime = Date.now()
+  return endTime-startTime
 }
 
 // Function to create and start a worker thread for a given method
@@ -70,12 +223,44 @@ function simulateMethod(methodId) {
   let result;
   switch (methodId) {
     case 1:
-      result = method1();
+      result = insertMemo();
       break;
     case 2:
-      result = method2();
+      result = insertDrug();
       break;
-    // Add more cases for additional methods if needed
+    case 3:
+        result = deleteDrug()
+        break;
+    case 4:
+        result = deleteTask()
+        break;
+    case 5:
+        result = getMedia()
+        break;
+    case 6:
+        result = getPazientiHome()
+        break;
+    case 7:
+        result = getInfoUser()
+        break;
+    case 8:
+        result = insertAlerts()
+        break;
+    case 9:
+        result = createOtp()
+        break;
+    case 10:
+        result = checkOtp()
+        break;
+    case 11:
+         result = getDataFromMongoDb()
+         break;
+    case 12:
+         result = insertPvs()
+         break;
+    case 3:
+         result = signUp()
+         break;
     default:
       result = { message: `Unknown method: ${methodId}` };
   }
@@ -109,7 +294,7 @@ if (!isMainThread) {
 
   // Simulate calling all methods for each worker thread
   for (let j = 1; j <= NUM_METHODS; j++) {
-    simulateMethod(j);
+    simulateMethod(methodId);
   }
 
   // Notify the main thread that all methods are completed
